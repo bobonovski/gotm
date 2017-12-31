cbe7f818d13ae4fd581bb568f98249e0823a4b22
bobonovski <zhaocc.act@gmail.com>
2018-01-01 01:52:19 +0800

    add doc-word topic book-keeping data structure

--- table/table.go
+++ table/table.go
@@ -12,13 +12,22 @@ var (
 	// vector of length topicNum: [t]-th element counts
 	// how many words in total has been assigned to topic t
 	WordTopicSum matrix.Matrix
+	// hashmap which remembers the topic of i-th word of doc d
+	// has been assigned before
+	DocWordTopic map[DocWord]uint32
 )
+
+type DocWord struct {
+	DocId   uint32
+	WordIdx uint32
+}
 
 // initialize word-topic and doc-topic sufficient statistics tables
 func Init(topicNum uint32, vocabSize uint32, docNum uint32) error {
 	WordTopic = matrix.NewDenseMatrix(vocabSize, topicNum)
 	DocTopic = matrix.NewDenseMatrix(docNum, topicNum)
 	WordTopicSum = matrix.NewDenseMatrix(topicNum, uint32(1))
+	DocWordTopic = make(map[DocWord]uint32)
 
 	return nil
 }
