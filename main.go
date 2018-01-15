31ac3422343bc73ada787820d1daf8a79ee660de
bobonovski <zhaocc.act@gmail.com>
2018-01-15 22:59:50 +0800

    add cmd argument

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 )
 
 var (
-	input      = flag.String("input_file", "", "input training file")
-	topicModel = flag.String("model", "lda", "model type")
-	alpha      = flag.Float64("alpha", 0.01, "document-topic mixture hyperparameter")
-	beta       = flag.Float64("beta", 0.01, "topic-word mixture hyperparameter")
-	topicNum   = flag.Uint("k", 20, "number of topics")
-	iteration  = flag.Int("iter", 10, "number of iteration")
+	input     = flag.String("input_file", "", "input training file")
+	modelType = flag.String("model_type", "lda", "model type")
+	alpha     = flag.Float64("alpha", 0.01, "document-topic mixture hyperparameter")
+	beta      = flag.Float64("beta", 0.01, "topic-word mixture hyperparameter")
+	topicNum  = flag.Uint("k", 20, "number of topics")
+	iteration = flag.Int("iter", 10, "number of iteration")
+	modelName = flag.String("model_file", "lda_model", "input/output model name")
 )
 
 func main() {
@@ -26,7 +27,7 @@ func main() {
 
 	// init model
 	var m model.Model
-	switch *topicModel {
+	switch *modelType {
 	case "lda":
 		m = model.NewLDA(data, uint32(*topicNum), float32(*alpha), float32(*beta))
 	case "sparselda":
@@ -35,5 +36,9 @@ func main() {
 		log.Printf("not supported yet")
 	}
 
+	// run model
 	m.Run(*iteration)
+
+	// save model
+	m.SaveModel(*modelName)
 }
